62677e9ac80ff1d7b07686dd609ee58a50c8de55
Juwon Bae <igg1159@autocrypt.io>
2023-06-09 22:04:54 +0900

    sample

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,23 +1,74 @@
+// #include <iostream>
+// #include "spdlog/fmt/fmt.h"
+// #include "consoleStyle.hpp"
+// #include "color.hpp"
+
+// int main()
+// {
+//     fmt::print("This is the client dir.\n");
+
+//     std::string text = "Font test";
+
+//     std::string boldText = console_stlye::makeBold(text);
+//     std::string underlinedText = console_stlye::makeUnderlined(text);
+//     std::string italicText = console_stlye::makeItalic(text);
+//     std::string inverseText = console_stlye::makeInverse(text);
+
+//     std::cout << "Bold: " << boldText << std::endl;
+//     std::cout << "Underlined: " << underlinedText << std::endl;
+//     std::cout << "Italic: " << italicText << std::endl;
+//     std::cout << "Inverse: " << inverseText << std::endl;
+
+//     return 0;
+// }
+
 #include <iostream>
-#include "spdlog/fmt/fmt.h"
-#include "consoleStyle.hpp"
-#include "color.hpp"
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
 
-int main()
+#define PORT 7777
+
+int main(int argc, char const *argv[])
 {
-    fmt::print("This is the client dir.\n");
+    int sock = 0;
+    int valread;
+    struct sockaddr_in serv_addr;
+    const char *hello = "Hello from client";
+    char buffer[1024] = {0};
 
-    std::string text = "Font test";
+    // Create socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        std::cerr << "Socket creation error" << std::endl;
+        return -1;
+    }
 
-    std::string boldText = console_stlye::makeBold(text);
-    std::string underlinedText = console_stlye::makeUnderlined(text);
-    std::string italicText = console_stlye::makeItalic(text);
-    std::string inverseText = console_stlye::makeInverse(text);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
 
-    std::cout << "Bold: " << boldText << std::endl;
-    std::cout << "Underlined: " << underlinedText << std::endl;
-    std::cout << "Italic: " << italicText << std::endl;
-    std::cout << "Inverse: " << inverseText << std::endl;
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        std::cerr << "Invalid address/ Address not supported" << std::endl;
+        return -1;
+    }
+
+    // Connect to the server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        std::cerr << "Connection Failed" << std::endl;
+        return -1;
+    }
+
+    // Send message to the server
+    send(sock, hello, strlen(hello), 0);
+    std::cout << "Hello message sent" << std::endl;
+
+    // Receive message from the server
+    valread = read(sock, buffer, 1024);
+    std::cout << buffer << std::endl;
 
     return 0;
 }
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,8 +1,69 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <cstring>
 #include "spdlog/fmt/fmt.h"
+#include <unistd.h>
+
+#define PORT 7777
 
 int main()
 {
-    fmt::print("This is the server dir.\n");
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    const char *hello = "Hello from server";
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        std::cerr << "Socket creation error" << std::endl;
+        return -1;
+    }
+
+    // Attaching socket to the port 8080
+    int status = setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+    std::cout << status << std::endl;
+    if (status)
+    {
+        std::cerr << "Setsockopt error" << std::endl;
+        return -1;
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    // Bind the socket to the specified port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        std::cerr << "Bind failed" << std::endl;
+        return -1;
+    }
+
+    // Listen for incoming connections
+    if (listen(server_fd, 3) < 0)
+    {
+        std::cerr << "Listen error" << std::endl;
+        return -1;
+    }
+
+    // Accept incoming connections
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
+        std::cerr << "Accept error" << std::endl;
+        return -1;
+    }
+
+    // Receive message from the client
+    valread = read(new_socket, buffer, 1024);
+    std::cout << buffer << std::endl;
+
+    // Send message to the client
+    send(new_socket, hello, strlen(hello), 0);
+    std::cout << "Hello message sent" << std::endl;
+
     return 0;
 }
